c4e595fd2df6b5725d74b22ce2a67173aec648c1
Heeyeun <koheeyeun96@gmail.com>
2025-01-18 19:02:38 +0900

    style: layout max width 600px로

--- src/app/routes/Router.tsx
+++ src/app/routes/Router.tsx
@@ -12,12 +12,12 @@ export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<RootLayout />}>
-        <Route path="/" element={<HomePage />} />
+        <Route path="" element={<HomePage />} />
       </Route>
       <Route path="/auth" element={<AuthLayout />}>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/signin" element={<SignInPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="" element={<LandingPage />} />
+        <Route path="signin" element={<SignInPage />} />
+        <Route path="signup" element={<SignUpPage />} />
       </Route>
     </Routes>
   );
--- src/global/layouts/RootLayout.tsx
+++ src/global/layouts/RootLayout.tsx
@@ -4,14 +4,11 @@ import { Fragment } from 'react'; // 수정된 부분
 export const RootLayout = () => {
   return (
     <Fragment>
-      <main
-        className="w-full max-w-[600px] mx-auto p-3 mt-[60px]"
-        style={{
-          height: 'calc(100dvh - 60px)',
-        }}
-      >
-        <Outlet />
-      </main>
+      <div className="w-full h-dvh flex justify-center bg-bgCanvasWhite overflow-y-auto overflow-x-hidden">
+        <div className="w-full max-w-[600px]">
+          <Outlet />
+        </div>
+      </div>
     </Fragment>
   );
 };
