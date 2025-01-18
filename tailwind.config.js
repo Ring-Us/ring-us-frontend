d8ba8120978d1eebdfeb849c484857f305314b80
Heeyeun <koheeyeun96@gmail.com>
2025-01-18 19:13:08 +0900

    chore: primary color 등 tailwind 변수 지정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    theme: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        white: '#ffffff',
+        black: '#000000',
+        red: '#E70000',
+        green: '#00AF49',
+        primary: '#310EE0',
+        secondary: '#7e5bef',
+        // 추가적인 색상 정의
+      },
+      borderRadius: {
+        lg: '14px',
+        md: '10px',  // 10px - 2px
+        sm: '6px',  // 10px - 4px
+      },
+    },
   },
   plugins: [],
 };
