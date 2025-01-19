1d8b15f1f1b669fab78b6e40f902e822290ef698
Heeyeun <koheeyeun96@gmail.com>
2025-01-20 03:22:24 +0900

    style: progress 스타일 커스텀

--- src/home/pages/HomePage.tsx
+++ src/home/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import { AuthButton } from '@/auth/components/AuthButton';
+import { Progress } from '@/global/ui/progress';
 
 export default function HomePage() {
   return (
     <div className="min-h-screen flex flex-col">
       <h1 className="text-4xl font-bold text-center">Home Page</h1>
       <AuthButton>다음으로</AuthButton>
+      <Progress value={33} className=" h-[6px]" />
     </div>
   );
 }
