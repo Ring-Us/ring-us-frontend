67ae3020fe3b0b8030b51bed73e3c6aeb78173dd
Heeyeun <koheeyeun96@gmail.com>
2025-01-20 03:07:16 +0900

    feat: AuthButton 작성

--- src/home/pages/HomePage.tsx
+++ src/home/pages/HomePage.tsx
@@ -1,7 +1,10 @@
+import { AuthButton } from '@/auth/components/AuthButton';
+
 export default function HomePage() {
   return (
     <div className="min-h-screen flex flex-col">
       <h1 className="text-4xl font-bold text-center">Home Page</h1>
+      <AuthButton>다음으로</AuthButton>
     </div>
   );
 }
