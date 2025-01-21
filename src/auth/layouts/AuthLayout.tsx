a90d1dd59625fc8158f6f3710f9e62c3ed82de4d
Junsub Lee <lee980605@knu.ac.kr>
2025-01-21 15:44:27 +0900

    style: Layout 디자인 shadow 추가

--- src/auth/layouts/AuthLayout.tsx
+++ src/auth/layouts/AuthLayout.tsx
@@ -5,7 +5,7 @@ export const AuthLayout = () => {
   return (
     <Fragment>
       <div className="w-full h-dvh flex justify-center bg-bgCanvasWhite overflow-y-auto overflow-x-hidden">
-        <div className="w-full max-w-[600px]">
+        <div className="w-full max-w-[600px] shadow-lg bg-bgPrimaryWhite">
           <Outlet />
         </div>
       </div>
